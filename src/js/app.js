6a933573d4b0f354afd77780c5cb620fcd3f4ae5
umesh c <chaurasiyaumesh9@gmail.com>
2019-11-08 13:42:29 +0530

    fontawesome added

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 import '../scss/app.scss';
 import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 // Your JS Code goes here
