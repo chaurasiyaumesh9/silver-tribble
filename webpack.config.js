d20fa2d8b67b9ed5e5620606fc6883f65a1b965b
umesh c <chaurasiyaumesh9@gmail.com>
2019-11-11 16:12:33 +0530

    anther ammendments

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const config = {
   },
   output: {
     filename: "js/[name].js",
-    path: path.resolve(__dirname, "/dist")
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
